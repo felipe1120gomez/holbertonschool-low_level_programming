c29ec2e6841b0c51f4639ae2df283a8191a6eac8
felipe1120gomez <felipe92agudelo@gmail.com>
2020-09-17 21:38:56 +0000

    Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10
+ *
+ * Description: putchar function
+ * Return: zero
+ */
+int main(void)
+{
+
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+		if (c < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
