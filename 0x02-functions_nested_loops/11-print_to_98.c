2f784caf47bc427968af3d6e50bc70a679b29493
felipe1120gomez <felipe92agudelo@gmail.com>
2020-09-22 19:41:32 +0000

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - numbers from n to 98 in order
+ * @n: first number
+ * Return: total
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("%d\n", n);
+}
