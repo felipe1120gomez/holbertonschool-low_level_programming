84ae258cc235abae03b7c869383a62024fa52813
felipe1120gomez <felipe92agudelo@gmail.com>
2020-09-24 23:04:10 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: Always 0, always success!
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 15))
+			printf("FizzBuzz ");
+		else if (!(i % 5))
+			printf("Buzz ");
+		else if (!(i % 3))
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
