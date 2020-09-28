808ad596793a475df9a7bb25b7d6194d5e4839a7
felipe1120gomez <felipe92agudelo@gmail.com>
2020-09-28 15:50:16 +0000

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - set value to 98
+ * @n: pointer to a variable in the calling function
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
